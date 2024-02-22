be320fd29206d13e504b72d31d93df89e398a235
Steven Silvester <steven.silvester@ieee.org>
2024-02-22 15:49:18 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ set -eux
 
 python3 -m venv .venv
 source .venv/bin/activate
-pip install git+https://github.com/blink1073/mongo-python-driver.git@PYTHON-3467-2
+pip install git+https://github.com/mongodb/mongo-python-driver.git
 pip install .
