ccd11a14602df2974a81231246c7ecbd0666bddd
Steven Silvester <steve.silvester@mongodb.com>
2024-03-15 07:39:44 -0500

    fix token handling

--- test_custom_callback.py
+++ test_custom_callback.py
@@ -10,7 +10,7 @@ uri = os.environ['MONGODB_URI']
 class MyCallback(OIDCCallback):
     def fetch(self, context: OIDCCallbackContext) -> OIDCCallbackResult:
         credential = DefaultAzureCredential(managed_identity_client_id=client_id)
-        token = credential.get_token(f"api://{app_id}/.default")
+        token = credential.get_token(f"api://{app_id}/.default").token
         return OIDCCallbackResult(access_token=token)
 
 props = dict(OIDC_CALLBACK=MyCallback())
--- test_provider.py
+++ test_provider.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 client_id = os.environ['AZURE_APP_CLIENT_ID']
-username = os.environ['AZURE_IDENTITY_OBJECT_ID']
+username = os.environ['AZURE_IDENTITY_CLIENT_ID']
 mech_props=dict(PROVIDER_NAME="azure",TOKEN_AUDIENCE=f"api%3A%2F%2F{client_id}")
 kwargs = dict(authmechanismproperties=mech_props, username=username)
 client = MongoClient(os.environ['MONGODB_URI'], **kwargs)
