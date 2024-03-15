488a644f8007d96379ac3a763a453fc4c5870848
Steven Silvester <steve.silvester@mongodb.com>
2024-03-15 09:58:41 -0500

    update username

--- test_provider.py
+++ test_provider.py
@@ -1,9 +1,9 @@
 import os
 from pymongo import MongoClient
 
-client_id = os.environ['AZURE_APP_CLIENT_ID']
+app_id = os.environ['AZURE_APP_CLIENT_ID']
 username = os.environ['AZURE_IDENTITY_CLIENT_ID']
-mech_props=dict(PROVIDER_NAME="azure",TOKEN_AUDIENCE=f"api%3A%2F%2F{client_id}")
+mech_props=dict(PROVIDER_NAME="azure",TOKEN_AUDIENCE=f"api%3A%2F%2F{app_id}")
 kwargs = dict(authmechanismproperties=mech_props, username=username)
 client = MongoClient(os.environ['MONGODB_URI'], **kwargs)
 client.test.test.insert_one({})
